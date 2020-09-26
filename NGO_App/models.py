e410b70e97b3eaf8b699ff40c6147b0d528a355c
Your Name <you@example.com>
2020-09-26 18:23:37 +0530

    first commit

---
+++ NGO_App/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class NGOProfile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    registration_number=models.CharField(unique=True,max_length=10)
+    ngo_name=models.CharField(max_length=50)
+    certificate=models.ImageField(blank=False,upload_to='certificate_pics')
+    contact = models.CharField(max_length=10)
+    country=models.CharField(max_length=50)
+    state=models.CharField(max_length=50)
+    city=models.CharField(max_length=50)
+    pincode=models.CharField(max_length=50)
+    verfied=models.BooleanField(default=False)
+    address_line_1=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    contact = models.CharField(max_length=10)
+    country=models.CharField(max_length=50)
+    state=models.CharField(max_length=50)
+    city=models.CharField(max_length=50)
+    pincode=models.CharField(max_length=50)
+    address_line_1=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
+
+class NgoRequirements(models.Model):
+    ngo = models.ForeignKey(NGOProfile,on_delete=models.CASCADE)
+    requirement= models.CharField(max_length=60,null=False)
+    quantity=models.IntegerField(null=False)
+    message=models.CharField(max_length=300)
+
+    def __str__(self):
+        return str(self.id)
+
+class Receipt(models.Model):
+    ngo=models.ForeignKey(NGOProfile,on_delete=models.CASCADE)
